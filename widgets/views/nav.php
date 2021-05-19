b0c6e8530d1a1ac0afef6ec07757545a4fc932c7
Zhandos <zhandos38@gmail.com>
2021-05-19 21:48:45 +0600

    fixes

--- widgets/views/nav.php
+++ widgets/views/nav.php
@@ -14,7 +14,7 @@ use yii\helpers\Url;
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
-                    <li class="nav-item"><a class="nav-link" href="index-2.html">Басты бет</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/">Басты бет</a></li>
                     <!-- end of nav-item -->
                     <li class="nav-item dropdown active">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
